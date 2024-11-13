1f98001b028ce2e970db5bb5e9272137ef362209
atishya29 <legendsfanclub17@gmail.com>
2024-11-13 06:43:17 +0000

    understand basics of datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treatt all js code as newer version of js
+
+// alert(3 + 3)  // we are using nodejs not browser, have to use different syntax for alert
+
+console.log(3 
+    +
+     3) // code readiability should be high this one will run but not a good practice
+
+console.log("AtishyaJain")
+
+// Doccumentation to follow-
+// MDN and tc39.es  ecma script
+
+let name = "Atishya" //string
+let age = 18 //number
+let isLoggedIn = false //boolean
+let state; //undefined
+
+
+// DataTypes-
+// numbers => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone vale
+// undefined => value not assigned
+// symbol (it will implement more when we will do react, used to find uniqueness)
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+// it implies undefined is itself a type but null is an object
+
