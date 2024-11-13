316d495f51922a4a56c0c6edd8e21abacbe898e9
atishya29 <legendsfanclub17@gmail.com>
2024-11-13 07:48:37 +0000

    Learned more about conversions and opperators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,6 +29,57 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+// ******************** Operations ************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//baisc arithmatic operations-
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " atishya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // to understand this visit "https://tc39.es/ecma262/2023/multipage/ 
+//  abstract-operations.html#sec-type-conversion"  abhi sirf itna yaad rakh agr string first hai to 
+// sabi ko string m treat kra jaayega vrna starting ka opperation hojyega baad m string ki tarah treat 
+// hoga 
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3); //use () to make it specific which operation should take place first
+// these all are not good practices code readiability is negative
+
+// console.log(+true);
+// console.log(+"");
+// ye dono tricky conversions h we should avoid these but we have to study Operator precedence
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter); // yaha dono m koi chng ni aaya but chnges aate hai 
+// read about prefix and postfix opperator at mdn
+
+
+
+
 
