1bf7c8d272e299c10c5a8e187a3e9bca63d482fd
atishya29 <legendsfanclub17@gmail.com>
2024-11-15 08:09:17 +0000

    half video of string completed

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "atishya"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+// concatenate k karne ka ye syntax acha ni hai i.e, bohot outdated hai
+
+
+// in present time use, backticks `` benifits of using it is
+// it supports String Interpolation i.e you create placeholder and can directly inject variables in them
+// Syntax of String Interpolation
+// you can insert variables or expressions using ${expression}, where:
+// ${} is the placeholder for the expression.
+// expression is any valid JavaScript expression.
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+
