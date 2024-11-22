a4f6017978cb5e831485838816688ea8cab61bb4
atishya29 <legendsfanclub17@gmail.com>
2024-11-22 05:58:57 +0000

    basics of scope started

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,61 @@
+// {} ise hi bolte hai scope object m jo {} use hote h unhe object declaration kehte h ye  jab functions k sath aata h ya if else k sath tab ise bola jata h scope i.e scope of that program ya function
+
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+} 
